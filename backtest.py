f1ceb593faf3f64356cdc3d279c542c4e3fa007d
Peter Lau <peterlau0010@gmail.com>
2019-03-26 23:51:56 +0800

    add LoadHistory and update backtest

--- backtest.py
+++ backtest.py
@@ -1,18 +1,83 @@
-import config as cfg 
-import pandas as pd
-import datetime
-from binance.client import Client
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
 
-client = Client(cfg.binanceKey['api_key'], cfg.binanceKey['api_secret'])
+import datetime  # For datetime objects
+import os.path  # To manage paths
+import sys  # To find out the script name (in argv[0])
 
-symbol ='BNBUSDT'
+# Import the backtrader platform
+import backtrader as bt
 
-cryptocurrency = client.get_historical_klines(symbol, Client.KLINE_INTERVAL_1MINUTE, "1 day ago UTC")
 
-cryptocurrency = pd.DataFrame(cryptocurrency, columns=['Open Time','Open','High','Low','Close','Volumn','Close time','Quote asset volume','Number of trades','Taker buy base asset volume','Taker buy quote asset volume','Ignore'])
+# Create a Stratey
+class TestStrategy(bt.Strategy):
 
-cryptocurrency['Open Time'] = pd.to_datetime(cryptocurrency['Open Time'],unit='ms')
+    def log(self, txt, dt=None):
+        ''' Logging function fot this strategy'''
+        dt = dt or self.datas[0].datetime.date(0)
+        print('%s, %s' % (dt.isoformat(), txt))
 
-cryptocurrency.set_index('Open Time', inplace=True)
+    def __init__(self):
+        # Keep a reference to the "close" line in the data[0] dataseries
+        self.dataclose = self.datas[0].close
 
-cryptocurrency.to_csv( str(symbol) + '_'+ str(datetime.datetime.now().strftime('%Y_%m_%d')), date_format='%Y-%m-%d %H:%M:%S')
+    def next(self):
+        # Simply log the closing price of the series from the reference
+        self.log('Close, %.2f' % self.dataclose[0])
+
+        if self.dataclose[0] < self.dataclose[-1]:
+            # current close less than previous close
+
+            if self.dataclose[-1] < self.dataclose[-2]:
+                # previous close less than the previous close
+
+                # BUY, BUY, BUY!!! (with all possible default parameters)
+                self.log('BUY CREATE, %.2f' % self.dataclose[0])
+                self.buy()
+
+
+if __name__ == '__main__':
+    # Create a cerebro entity
+    cerebro = bt.Cerebro()
+
+    # Add a strategy
+    cerebro.addstrategy(TestStrategy)
+
+    # Datas are in a subfolder of the samples. Need to find where the script is
+    # because it could have been called from anywhere
+    modpath = os.path.dirname(os.path.abspath(sys.argv[0]))
+    datapath = os.path.abspath(os.getcwd() + '/BNBUSDT_' + str(datetime.datetime.now().strftime("%Y_%m_%d")))
+
+    # Create a Data Feed
+    data = bt.feeds.GenericCSVData(
+        dataname=datapath,
+        fromdate=datetime.datetime(2018, 1, 1),
+        dtformat=('%Y-%m-%d %H:%M:%S'),
+        datetime=0,
+        high=2,
+        low=3,
+        open=1,
+        close=4,
+        volume=5,
+        openinterest=-1,
+        timeframe= bt.TimeFrame.Minutes,
+        compression= 1
+    )
+
+    # Add the Data Feed to Cerebro
+    cerebro.adddata(data)
+
+    # Set our desired cash start
+    cerebro.broker.setcash(100000.0)
+
+    # Print out the starting conditions
+    print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
+
+    # Run over everything
+    cerebro.run()
+
+    # Print out the final result
+    print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
+    # datapath = os.path.abspath(os.getcwd() + '/BNBUSDT_' + str(datetime.datetime.now().strftime("%Y_%m_%d")))
+
+    cerebro.plot()
