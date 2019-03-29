bfc450113927e27f72702fffaa16752fc5159e0a
Peter Lau <peterl@hk1.ibm.com>
2019-03-29 18:09:17 +0800

    update

--- backtest.py
+++ backtest.py
@@ -7,22 +7,32 @@ import sys  # To find out the script name (in argv[0])
 
 # Import the backtrader platform
 import backtrader as bt
-import config as cfg 
+import backtrader.indicators as btind
+import backtrader.feeds as btfeeds
+
+# config
+import config as cfg
 
 # Create a Stratey
+
+
 class TestStrategy(bt.Strategy):
     params = (
-        ('smaShort', 10),
-        ('smaLong', 20),
-        ('emaShort', 10),
-        ('emaLong', 20),
-        ('rsiPeriod', 10),
+        ('maperiod', 15),
+        ('printlog', True),
+        ('stoptype', bt.Order.StopTrail),
+        ('trailamount', 0.0),
+        ('trailpercent', 0.02),
+        ('macd1',12),
+        ('macd2',26),
+        ('macdsig',9),
     )
 
-    def log(self, txt, dt=None):
+    def log(self, txt, dt=None, doprint=False):
         ''' Logging function fot this strategy'''
-        dt = dt or self.datas[0].datetime.date(0)
-        print('%s, %s' % (dt.isoformat(), txt))
+        if self.params.printlog or doprint:
+            dt = dt or self.datas[0].datetime.date(0)
+            print('%s, %s' % (dt.isoformat(), txt))
 
     def __init__(self):
         # Keep a reference to the "close" line in the data[0] dataseries
@@ -34,37 +44,19 @@ class TestStrategy(bt.Strategy):
         self.buycomm = None
 
         # Add a MovingAverageSimple indicator
-        self.smaShort = bt.indicators.SimpleMovingAverage(
-            self.datas[0], period=self.params.smaShort)
-
-        self.smaLong = bt.indicators.SimpleMovingAverage(
-            self.datas[0], period=self.params.smaLong)
-
-        # self.emaShort = bt.indicators.EMA(period=self.params.emaShort)
-
-        self.emaLong = bt.indicators.EMA(period=self.params.emaLong)
-
-        self.psar = bt.indicators.ParabolicSAR()
-
-        #self.rsi = bt.indicators.RelativeMomentumIndex(upperband=80, lowerband =20, period=self.params.rsiPeriod)
-
-        self.adx = bt.indicators.AverageDirectionalMovementIndex()  
-
-        self.buySignal = []
-        self.sellSignal = []
-        self.buySignal.append(bt.indicators.CrossUp(self.data, self.emaLong))
-        self.buySignal.append(self.data > self.psar)
-        # self.buySignal.append(self.smaShort > self.smaLong)
-        self.buySignal.append(self.adx > 30)
-        #self.sellSignal = bt.indicators.CrossUp(self.psar, self.emaShort)
-        # self.sellSignal2 = self.rsi > 90
-        self.sellSignal.append(self.psar > self.data)
-
+        self.sma = btind.SimpleMovingAverage(self.data, period=self.p.maperiod)
+        self.macd = bt.indicators.MACD(self.data,
+                                       period_me1=self.p.macd1,
+                                       period_me2=self.p.macd2,
+                                       period_signal=self.p.macdsig)
+        self.psar = bt.indicators.ParabolicSAR(self.data) 
 
     def notify_order(self, order):
+        # self.log('An order new/changed/executed/canceled has been received')
         if order.status in [order.Submitted, order.Accepted]:
             # Buy/Sell order submitted/accepted to/by broker - Nothing to do
             return
+
         # Check if an order has been completed
         # Attention: broker could reject order if not enough cash
         if order.status in [order.Completed]:
@@ -88,6 +80,7 @@ class TestStrategy(bt.Strategy):
         elif order.status in [order.Canceled, order.Margin, order.Rejected]:
             self.log('Order Canceled/Margin/Rejected')
 
+        # Write down: no pending order
         self.order = None
 
     def notify_trade(self, trade):
@@ -99,7 +92,7 @@ class TestStrategy(bt.Strategy):
 
     def next(self):
         # Simply log the closing price of the series from the reference
-        #self.log('Close, %.2f' % self.dataclose[0])
+        # self.log('Close, %.2f' % self.dataclose[0])
 
         # Check if an order is pending ... if yes, we cannot send a 2nd one
         if self.order:
@@ -107,34 +100,58 @@ class TestStrategy(bt.Strategy):
 
         # Check if we are in the market
         if not self.position:
+
             # Not yet ... we MIGHT BUY if ...
-            if all(self.buySignal) and self.adx[0]> self.adx[-1]:
+            if self.macd > self.data.lines.close[0]:
+
                 # BUY, BUY, BUY!!! (with all possible default parameters)
                 self.log('BUY CREATE, %.2f' % self.dataclose[0])
 
                 # Keep track of the created order to avoid a 2nd order
                 self.order = self.buy()
 
-        else:
-            if any(self.sellSignal):
-                # SELL, SELL, SELL!!! (with all possible default parameters)
-                self.log('SELL CREATE, %.2f' % self.dataclose[0])
+        elif self.psar > self.data.lines.close[0]:
+            # SELL, SELL, SELL!!! (with all possible default parameters)
+            self.log('SELL CREATE, %.2f' % self.dataclose[0])
 
-                # Keep track of the created order to avoid a 2nd order
-                self.order = self.sell()
-            elif (self.buyprice - self.data.low) > 20:
-                print(self.buyprice)
-                print(self.data.low)
-                self.log('SELL CREATE stop loss, %.2f' % self.dataclose[0])
-                self.order = self.sell()
+            # Keep track of the created order to avoid a 2nd order
+            self.order = self.sell()
+
+        elif self.order is None:
+            self.log('SELL CREATE Stop lose, %.2f' % self.dataclose[0])
+            self.order = self.sell(exectype=self.p.stoptype,
+                                   trailamount=self.p.trailamount,
+                                   trailpercent=self.p.trailpercent)
+
+            if self.p.trailamount:
+                tcheck = self.data.close - self.p.trailamount
+            else:
+                tcheck = self.data.close * (1.0 - self.p.trailpercent)
+        else:
+            if self.p.trailamount:
+                tcheck = self.data.close - self.p.trailamount
+            else:
+                tcheck = self.data.close * (1.0 - self.p.trailpercent)
+            print(','.join(
+                map(str, [self.datetime.date(), self.data.close[0],
+                          self.order.created.price, tcheck])
+            )
+            )
+
+    def stop(self):
+        self.log('(MA Period %2d) Ending Value %.2f' %
+                 (self.params.maperiod, self.broker.getvalue()), doprint=True)
+
 
 if __name__ == '__main__':
     # Create a cerebro entity
     cerebro = bt.Cerebro()
 
     # Add a strategy
+    # strats = cerebro.optstrategy(
+    #     TestStrategy,
+    #     maperiod=range(10, 31))
     cerebro.addstrategy(TestStrategy)
-
     # Datas are in a subfolder of the samples. Need to find where the script is
     # because it could have been called from anywhere
     modpath = os.path.dirname(os.path.abspath(sys.argv[0]))
@@ -154,17 +171,17 @@ if __name__ == '__main__':
         volume=5,
         openinterest=-1,
         timeframe=bt.TimeFrame.Minutes,
-        compression=1
+        compression=240
     )
 
     # Add the Data Feed to Cerebro
     cerebro.adddata(data)
 
     # Set our desired cash start
-    cerebro.broker.setcash(100000.0)
+    cerebro.broker.setcash(500.0)
 
     # Add a FixedSize sizer according to the stake
-    cerebro.addsizer(bt.sizers.PercentSizer, percents=50)
+    cerebro.addsizer(bt.sizers.PercentSizer, percents=90)
 
     # Set the commission
     cerebro.broker.setcommission(commission=0.00075)
