9f149f82f1a79bf0303b7032a90aa97f92947729
Peter Lau <peterl@hk1.ibm.com>
2019-04-10 18:15:28 +0800

    update

--- TensorflowTest.py
+++ TensorflowTest.py
@@ -1,180 +1,133 @@
-# %%
-import matplotlib.pyplot as plt
-from keras.layers import LSTM
-from keras.layers import Dense
-from keras.models import Sequential
+# Importing the libraries
 import numpy as np
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.metrics import mean_squared_error
-from sklearn.model_selection import train_test_split
-import config as cfg
+import matplotlib.pyplot as plt
 import pandas as pd
 import datetime
-from binance.client import Client
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+from keras.models import load_model
 import math
-# %matplotlib inline
+from numpy import array
 
-
-# client = Client(cfg.binanceKey['api_key'], cfg.binanceKey['api_secret'])
-# symbol = cfg.currency['currency']
-# cryptocurrency = client.get_historical_klines(symbol, Client.KLINE_INTERVAL_1DAY, "1 Jan, 2018 UTC", "1 Jan, 2019")
-# cryptocurrency = pd.read_csv('BNBUSDT_2019_04_08', engine='python')
-# cryptocurrency = pd.DataFrame(cryptocurrency, columns=['Open Time', 'Open', 'High', 'Low', 'Close', 'Volumn', 'Close time',
-                                                    #    'Quote asset volume', 'Number of trades', 'Taker buy base asset volume', 'Taker buy quote asset volume', 'Ignore'])
-# cryptocurrency['Open Time'] = pd.to_datetime(
-# cryptocurrency['Open Time'], unit='ms')
-# cryptocurrency.set_index('Open Time', inplace=True)
-# cryptocurrency['Close'] = cryptocurrency['Close'].astype(float)
-# cryptocurrency['Close'].plot(figsize=(20, 10), title='1 year price')
-
-# data = cryptocurrency.iloc[:, 3:4].astype(float).values
-
-
-# scaler = MinMaxScaler()
-
-# data = scaler.fit_transform(data)
-
-# training_set = data[:int((len(data)/2))]
-# test_set = data[int((len(data))/2):]
-# # training_set = data[:8600]
-# # test_set = data[8600:]
-
-# X_train = training_set[0:len(training_set) - 1]
-# y_train = training_set[1:len(training_set)]
-
-# X_test = test_set[0:len(test_set)-1]
-# y_test = test_set[1:len(test_set)]
-
-
-# X_train = np.reshape(X_train, (len(X_train), 1, X_train.shape[1]))
-# X_test = np.reshape(X_test, (len(X_test), 1, X_test.shape[1]))
-
-
-# model = Sequential()
-# model.add(LSTM(256, return_sequences=True, input_shape=(
-#     X_train.shape[1], X_train.shape[2])))
-
-# model.add(LSTM(256))
-# model.add(Dense(1))
-# model.compile(loss='mean_squared_error', optimizer='adam')
-# model.fit(X_train, y_train, epochs=50, batch_size=16, shuffle=False)
-# # print(X_test)
-# predicted_price = model.predict(X_test)
-# # print(predicted_price)
-# predicted_price = scaler.inverse_transform(predicted_price)
-# real_price = scaler.inverse_transform(y_test)
-
-
-# print('Price for last 5 : ')
-# print(predicted_price[-5:])
-# future_price = model.predict(np.asarray([[predicted_price[-1]]]))
-# future_price = scaler.inverse_transform(future_price)
-# print('Next price is ', future_price)
-
-# plt.figure(figsize=(20, 8))
-# plt.plot(predicted_price, color='red', label='Predicted Price')
-# plt.plot(real_price, color='blue', label='Real Price')
-# # plt.plot(future_price, color='green', label='Future Price')
-# plt.title('Predicted vs. Real Price')
-# plt.xlabel('Time')
-# plt.ylabel('Price')
+dataset = pd.read_csv('BNBUSDT_2019_04_09', index_col="Open Time",
+                      parse_dates=True, engine='python', usecols=[0, 4],)
+# plt.plot(dataset)
 # plt.show()
-# normalize the dataset
-
-# convert an array of values into a dataset matrix
-def create_dataset(dataset):
-    dataX, dataY = [], []
-    for i in range(len(dataset)-1):
-        dataX.append(dataset[i])
-        dataY.append(dataset[i + 1])
-    return np.asarray(dataX), np.asarray(dataY)
-
 
 # fix random seed for reproducibility
 np.random.seed(7)
 
 # load the dataset
-df = pd.read_csv('BNBUSDT_2019_04_08', engine='python')
-df.set_index('Open Time', inplace=True)
-df['Close'] = df['Close'].astype(float)
-# df = df.iloc[::-1]
-# df = df.drop(['Open Time', 'Open', 'High', 'Low',
-            #   'Volumn', 'Close time', 'Quote asset volume', 'Number of trades', 'Taker buy base asset volume', 'Taker buy quote asset volume', 'Ignore'], axis=1)
-# dataset = df.values
-dataset = df.iloc[:, 3:4].astype(float).values
+dataframe = pd.read_csv('BNBUSDT_2019_04_09', index_col="Open Time",
+                        parse_dates=True, engine='python', usecols=[0, 4],)
+dataset = dataframe.values
+dataset = dataset.astype('float32')
 
+# normalize the dataset
 scaler = MinMaxScaler(feature_range=(0, 1))
 dataset = scaler.fit_transform(dataset)
 
-# prepare the X and Y label
-X, y = create_dataset(dataset)
+# split into train and test sets
+train_size = int(len(dataset) * 1)
+test_size = len(dataset) - train_size
+train, test = dataset[0:train_size, :], dataset[train_size:len(dataset), :]
+print(len(train), len(test))
 
-# Take 80% of data as the training sample and 20% as testing sample
-trainX, testX, trainY, testY = train_test_split(
-    X, y, test_size=0.20, shuffle=False)
+# convert an array of values into a dataset matrix
+
+
+def create_dataset(dataset, look_back=1):
+    dataX, dataY = [], []
+    for i in range(len(dataset)-look_back-1):
+        a = dataset[i:(i+look_back), 0]
+        dataX.append(a)
+        dataY.append(dataset[i + look_back, 0])
+    return np.array(dataX), np.array(dataY)
+
+# reshape into X=t and Y=t+1
+look_back = 5
+trainX, trainY = create_dataset(train, look_back)
+# testX, testY = create_dataset(test, look_back)
 
 # reshape input to be [samples, time steps, features]
+
 trainX = np.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
-testX = np.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
+print('trainX 1 ', np.shape(trainX))
+# testX = np.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
 
 # create and fit the LSTM network
-# model = Sequential()
-# model.add(LSTM(4, input_shape=(1, 1)))
-# model.add(Dense(1))
-# model.compile(loss='mean_squared_error', optimizer='adam')
 model = Sequential()
-model.add(LSTM(256, return_sequences=True, input_shape=(
-    trainX.shape[1], trainX.shape[2])))
-
-model.add(LSTM(256))
+model.add(LSTM(4, input_shape=(1, look_back)))
 model.add(Dense(1))
 model.compile(loss='mean_squared_error', optimizer='adam')
 model.fit(trainX, trainY, epochs=5, batch_size=1, verbose=2)
 
 # save model for later use
-model.save('./savedModel')
+# model.save('./savedModel')
 
 # load_model
 # model = load_model('./savedModel')
 
 # make predictions
+# print('trainX 2', np.shape(trainX))
 trainPredict = model.predict(trainX)
-testPredict = model.predict(testX)
-
-futurePredict = model.predict(np.asarray([[testPredict[-1]]]))
-futurePredict = scaler.inverse_transform(futurePredict)
-
-# invert predictions
+print('trainPredict', np.shape(trainPredict))
 trainPredict = scaler.inverse_transform(trainPredict)
-trainY = scaler.inverse_transform(trainY)
-testPredict = scaler.inverse_transform(testPredict)
-testY = scaler.inverse_transform(testY)
-
-realPrice = scaler.inverse_transform(dataset)
-
-print("Price for last 5 days: ")
-print(testPredict[-5:])
-print(realPrice[-5:])
-print("Bitcoin price for tomorrow: ", futurePredict)
-
-# calculate root mean squared error
-trainScore = math.sqrt(mean_squared_error(trainY[:, 0], trainPredict[:, 0]))
+trainY = scaler.inverse_transform([trainY])
+trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:, 0]))
 print('Train Score: %.2f RMSE' % (trainScore))
-testScore = math.sqrt(mean_squared_error(testY[:, 0], testPredict[:, 0]))
-print('Test Score: %.2f RMSE' % (testScore))
+print(trainPredict[-1:])
+
+yhat = model.predict(trainX, verbose=0)
+yhat = scaler.inverse_transform(yhat)
+print(yhat[-1:])
+
+# testPredict = model.predict(testX)
+# invert predictions
+
+
+# testPredict = scaler.inverse_transform(testPredict)
+# testY = scaler.inverse_transform([testY])
+# calculate root mean squared error
+
+# testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
+# print('Test Score: %.2f RMSE' % (testScore))
+
+# realPrice = scaler.inverse_transform(dataset)
+# print('trainPredict', np.shape(trainPredict))
+# print(trainPredict[-5:])
+# print('realPrice', np.shape(realPrice))
+# print(realPrice[-5:])
+
+# furtherX = scaler.fit_transform(trainPredict)
+# print('furtherX 1', np.shape(furtherX))
+# furtherX = np.reshape(furtherX, (furtherX.shape[0], 1, 1))
+# print('furtherX 2', np.shape(furtherX))
+# furtherPrice = model.predict(furtherX, verbose=0)
+# print('furtherPrice', np.shape(furtherPrice))
+# furtherPrice = scaler.inverse_transform(furtherPrice)
+# print(furtherPrice[-5:])
 
 # shift train predictions for plotting
 trainPredictPlot = np.empty_like(dataset)
 trainPredictPlot[:, :] = np.nan
-trainPredictPlot[1:len(trainPredict)+1, :] = trainPredict
+trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
+# # # shift test predictions for plotting
+# # testPredictPlot = np.empty_like(dataset)
+# # testPredictPlot[:, :] = np.nan
+# # testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredict
 
-# shift test predictions for plotting
-testPredictPlot = np.empty_like(dataset)
-testPredictPlot[:, :] = np.nan
-testPredictPlot[len(trainPredict):len(dataset)-1, :] = testPredict
+# # # shift test predictions for plotting
+# furtherPricePlot = np.empty_like(dataset)
+# furtherPricePlot[:, :] = np.nan
+# furtherPricePlot[look_back:len(trainPredict)+look_back, :] = furtherPrice
 
-# plot baseline and predictions
+# # plot baseline and predictions
 plt.plot(scaler.inverse_transform(dataset))
-# plt.plot(trainPredictPlot)
-plt.plot(testPredictPlot)
+plt.plot(trainPredictPlot)
+# plt.plot(furtherPricePlot)
+# # plt.plot(testPredictPlot)
 plt.show()
--- backtest.py
+++ backtest.py
@@ -1,237 +1,34 @@
-# %%
-from __future__ import (absolute_import, division, print_function,
-                        unicode_literals)
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from numpy import array
+from keras.models import load_model
 
-import datetime  # For datetime objects
-import os.path  # To manage paths
-import sys  # To find out the script name (in argv[0])
-import matplotlib
+# return training data
+def get_train():
+    seq = [[0.0, 0.1], [0.1, 0.2], [0.2, 0.3], [0.3, 0.4], [0.4, 0.5]]
+    seq = array(seq)
+    X, y = seq[:, 0], seq[:, 1]
+    X = X.reshape((len(X), 1, 1))
+    return X, y
 
-# Import the backtrader platform
-import backtrader as bt
-import backtrader.indicators as btind
-import backtrader.feeds as btfeeds
+# define model
+model = Sequential()
+model.add(LSTM(10, input_shape=(1,1)))
+model.add(Dense(1, activation='linear'))
+# compile model
+model.compile(loss='mse', optimizer='adam')
+# fit model
+X,y = get_train()
+# model.fit(X, y, epochs=300, shuffle=False, verbose=0)
+# save model to single file
+# model.save('lstm_model.h5')
 
-# config
-import config as cfg
+# snip...
+# later, perhaps run from another script
 
-# Create a Stratey
-
-
-class TestStrategy(bt.Strategy):
-    params = (
-        ('kdPeriod', 6),
-        ('printlog', False),
-        ('stoptype', bt.Order.StopTrail),
-        ('trailamount', 0.0),
-        ('trailpercent', 0.02),
-        ('macd1', 12),
-        ('macd2', 26),
-        ('macdsig', 9),
-        ('percK', 20),
-        ('kdPercK',20),
-        ('kdPercD',20),
-        ('rsiShort',9)
-    )
-
-    def log(self, txt, dt=None, doprint=False):
-        if self.params.printlog or doprint:
-            dt = dt or self.datas[0].datetime.date(0)
-            print('%s, %s' % (dt.isoformat(), txt))
-
-    def __init__(self):
-        # Keep a reference to the "close" line in the data[0] dataseries
-        self.dataclose = self.datas[0].close
-
-        # To keep track of pending orders and buy price/commission
-        self.order = None
-        self.buyprice = None
-        self.buycomm = None
-
-        # Add a MovingAverageSimple indicator
-        # self.sma = btind.SimpleMovingAverage(self.data, period=self.p.maperiod)
-        self.macd = bt.indicators.MACD(self.data)
-        # self.psar = bt.indicators.ParabolicSAR(self.data)
-        self.kd = bt.indicators.StochasticFull(self.data,period=self.p.kdPeriod)
-        self.rsiShort = bt.indicators.RelativeStrengthIndex(self.data,period=self.p.rsiShort)
-        # self.rsi12 = bt.indicators.RelativeStrengthIndex(self.data,period=12)
-
-        # self.percKCrossoverPercD = bt.indicators.CrossOver(self.kd.lines.percK, self.kd.lines.percD)
-        # self.percKCrossDownPercD = bt.indicators.CrossDown(self.kd.lines.percK, self.kd.lines.percD)
-        # self.rsiCrossUp = bt.indicators.CrossUp(self.rsi6, self.rsi12)
-
-        self.buysingal = bt.And(
-            # self.macd.lines.macd > self.macd.lines.signal,
-            self.kd.lines.percK < 15,
-            self.kd.lines.percD < 15,
-            self.kd.lines.percK > self.kd.lines.percD,
-            # bt.indicators.CrossUp(self.kd.lines.percD, self.kd.lines.percK),
-            self.rsiShort.lines.rsi < 20, #RSI below 30, over sell, short turn use 9, long use 14
-            # self.dataclose > self.psar.lines.psar
-            
-            # self.rsiCrossUp,
-        )
-
-        self.sellsignal = bt.And(
-            # self.percKCrossDownPercD,
-            # self.kd.lines.percK < 80,
-            # self.macd.lines.macd < self.macd.lines.signal,
-            self.kd.lines.percD > 85,
-            self.kd.lines.percK > 85,
-            # bt.indicators.CrossDown(self.kd.lines.percD, self.kd.lines.percK),
-            self.rsiShort.lines.rsi > 70, #RSI over 70, over buy
-            # self.dataclose < self.psar.lines.psar
-        )
-
-    def notify_order(self, order):
-        # self.log('An order new/changed/executed/canceled has been received')
-        if order.status in [order.Submitted, order.Accepted]:
-            # Buy/Sell order submitted/accepted to/by broker - Nothing to do
-            return
-
-        # Check if an order has been completed
-        # Attention: broker could reject order if not enough cash
-        if order.status in [order.Completed]:
-            if order.isbuy():
-                self.log(
-                    'BUY EXECUTED, Price: %.2f, Cost: %.2f, Comm %.2f' %
-                    (order.executed.price,
-                     order.executed.value,
-                     order.executed.comm))
-
-                self.buyprice = order.executed.price
-                self.buycomm = order.executed.comm
-            else:  # Sell
-                self.log('SELL EXECUTED, Price: %.2f, Cost: %.2f, Comm %.2f' %
-                         (order.executed.price,
-                          order.executed.value,
-                          order.executed.comm))
-
-            self.bar_executed = len(self)
-
-        elif order.status in [order.Canceled, order.Margin, order.Rejected]:
-            self.log('Order Canceled/Margin/Rejected')
-
-        # Write down: no pending order
-        self.order = None
-
-    def notify_trade(self, trade):
-        if not trade.isclosed:
-            return
-
-        self.log('OPERATION PROFIT, GROSS %.2f, NET %.2f' %
-                 (trade.pnl, trade.pnlcomm))
-
-    def next(self):
-        # Simply log the closing price of the series from the reference
-        # self.log('Close, %.2f' % self.dataclose[0])
-
-        # Check if an order is pending ... if yes, we cannot send a 2nd one
-        if self.order:
-            return
-
-        # Check if we are in the market
-        if not self.position:
-
-            # Not yet ... we MIGHT BUY if ...
-            if self.buysingal:
-                if self.kd.lines.percK[-1] > self.kd.lines.percK[-2]:
-                    if self.kd.lines.percK[0] > self.kd.lines.percK[-1]:
-
-                        # BUY, BUY, BUY!!! (with all possible default parameters)
-                        self.log('BUY CREATE, %.2f' % self.dataclose[0])
-
-                        # Keep track of the created order to avoid a 2nd order
-                        self.order = self.buy()
-
-        else:
-
-            if self.sellsignal:
-                # SELL, SELL, SELL!!! (with all possible default parameters)
-                self.log('SELL CREATE, %.2f' % self.dataclose[0])
-
-                # Keep track of the created order to avoid a 2nd order
-                self.order = self.sell()
-
-        # elif self.order is None:
-        #     self.log('SELL CREATE Stop lose, %.2f' % self.dataclose[0])
-        #     self.order = self.sell(exectype=self.p.stoptype,
-        #                            trailamount=self.p.trailamount,
-        #                            trailpercent=self.p.trailpercent)
-
-        #     if self.p.trailamount:
-        #         tcheck = self.data.close - self.p.trailamount
-        #     else:
-        #         tcheck = self.data.close * (1.0 - self.p.trailpercent)
-        # else:
-        #     if self.p.trailamount:
-        #         tcheck = self.data.close - self.p.trailamount
-        #     else:
-        #         tcheck = self.data.close * (1.0 - self.p.trailpercent)
-        #     print(','.join(
-        #         map(str, [self.datetime.date(), self.data.close[0],
-        #                   self.order.created.price, tcheck])
-        #     )
-        #     )
-
-    def stop(self):
-        self.log('(KD Period %2d)  (KD percK %2d) (KD percD %2d) Ending Value %.2f' %
-                 (self.params.kdPeriod, self.params.kdPercK, self.params.kdPercD, self.broker.getvalue()), doprint=True)
-
-
-if __name__ == '__main__':
-    try:
-        # Create a cerebro entity
-        cerebro = bt.Cerebro()
-
-        # Add a strategy
-        # strats = cerebro.optstrategy(TestStrategy,kdPeriod=range(6, 10), kdPercK=range(10,30,5),kdPercD=range(10,30,5))
-        cerebro.addstrategy(TestStrategy)
-        # Datas are in a subfolder of the samples. Need to find where the script is
-        # because it could have been called from anywhere
-        modpath = os.path.dirname(os.path.abspath(sys.argv[0]))
-        datapath = os.path.abspath(
-            os.getcwd() + '/' + cfg.currency['currency'] + '_' + str(datetime.datetime.now().strftime("%Y_%m_%d")))
-
-        # Create a Data Feed
-        data = bt.feeds.GenericCSVData(
-            dataname=datapath,
-            fromdate=datetime.datetime(2018, 10, 1),
-            todate=datetime.datetime(2018, 12, 31),
-            dtformat=('%Y-%m-%d %H:%M:%S'),
-            datetime=0,
-            high=2,
-            low=3,
-            open=1,
-            close=4,
-            volume=5,
-            openinterest=-1,
-            timeframe=bt.TimeFrame.Minutes,
-            compression=1
-        )
-
-        # Add the Data Feed to Cerebro
-        cerebro.adddata(data)
-
-        # Set our desired cash start
-        cerebro.broker.setcash(500.0)
-
-        # Add a FixedSize sizer according to the stake
-        cerebro.addsizer(bt.sizers.PercentSizer, percents=90)
-
-        # Set the commission
-        cerebro.broker.setcommission(commission=0.00075)
-
-        # Print out the starting conditions
-        print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
-
-        # Run over everything
-        cerebro.run()
-
-        # Print out the final result
-        print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
-
-        # Plot the result
-        # cerebro.plot()
-    except Exception as ex:
-        print(ex)
+# load model from single file
+model = load_model('lstm_model.h5')
+# make predictions
+yhat = model.predict(X, verbose=0)
+print(yhat)
